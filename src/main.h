51ce752c36417a3411b8994c861d89d3533bdac2
akumaburn <a.eslampanah@live.com>
2013-07-21 03:10:20 -0400

    July Fork v009

--- src/main.h
+++ src/main.h
@@ -34,7 +34,7 @@ static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 static const int64 MIN_TX_FEE = 10000000;
 static const int64 MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64 MAX_MONEY = 119124000 * COIN; // GLDcoin: maximum of 119,124,000 coins in a single wallet... this value does not dictate maximum number of coins on its own.
-const int64 julyFork = 200000000;//29610//40000, remember to change this value to something very large like 200000000 if you want this client to behave as the old client
+const int64 julyFork = 29610;//29610//40000, remember to change this value to something very large like 200000000 if you want this client to behave as the old client
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 static const int COINBASE_MATURITY = 100;
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
