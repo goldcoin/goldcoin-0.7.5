10b91cc76b3f5680e81c6048cb46063ea9b9cca3
akumaburn <a.eslampanah@live.com>
2013-10-21 13:24:13 -0400

    7.1.1
    Fixes an issue when downloading the blockchain from scratch. Recommended
    update.

--- src/version.h
+++ src/version.h
@@ -16,7 +16,7 @@
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       7
 #define CLIENT_VERSION_REVISION    1
-#define CLIENT_VERSION_BUILD       0
+#define CLIENT_VERSION_BUILD       1
 
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
@@ -36,7 +36,7 @@ static const int PROTOCOL_VERSION = 60008;
 
 static const int MIN_PROTO_VERSION = 60004;
 // earlier versions not supported as of October 2013, and are disconnected
-// 60004 deprecates in a week! make sure to upgrade to 7.1 before then.
+// 60004 deprecates in a week! make sure to upgrade to 7.1.1 before then.
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
