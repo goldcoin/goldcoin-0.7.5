ff7689a0dfab337d74e14f5397fd6e17f078dd05
akumaburn <a.eslampanah@live.com>
2013-11-04 04:28:58 -0500

    7.1.4
    Fixes the difficulty code.

--- src/main.h
+++ src/main.h
@@ -35,6 +35,7 @@ static const int64 MIN_TX_FEE = 10000000;
 static const int64 MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64 MAX_MONEY = 123423900 * COIN; // GoldCoin (GLD): maximum of 123,423,900 coins in a single wallet... this value does not dictate maximum number of coins on its own.
 const int64 julyFork = 45000;
+const int64 novemberFork = 103000;
 const int octoberFork = 100000;
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 static const int COINBASE_MATURITY = 100;
@@ -47,6 +48,7 @@ static const int fHaveUPnP = false;
 #endif
 
 extern bool hardForkedJuly;
+extern bool hardForkedNovember;
 
 extern CScript COINBASE_FLAGS;
 
