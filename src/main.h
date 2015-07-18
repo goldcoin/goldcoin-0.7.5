de0f52656062df80a2db7df9df9581880425ef66
akumaburn <a.eslampanah@live.com>
2015-07-17 20:34:06 -0400

    Golden River - July Fork 2
    Difficulty Algorithm vastly changed.

--- src/main.h
+++ src/main.h
@@ -39,6 +39,8 @@ const int64 novemberFork = 103000;
 const int64 novemberFork2 = 118800;
 const int64 mayFork = 248000;
 const int octoberFork = 100000;
+
+const int64 julyFork2 = 253000;
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 static const int COINBASE_MATURITY = 100;
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
@@ -901,6 +903,11 @@ public:
     {
         return (int64)nTime;
     }
+	
+	int64 GetBits() const
+	{
+		return (int64)nBits;
+	}
 
     void UpdateTime(const CBlockIndex* pindexPrev);
 
