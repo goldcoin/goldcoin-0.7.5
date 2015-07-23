43e84d1d5f66d59e494f167b22e2ac441962fa2e
akumaburn <a.eslampanah@live.com>
2015-07-23 14:39:31 -0400

    7.2.0

--- src/version.h
+++ src/version.h
@@ -15,8 +15,8 @@
 // These need to be macro's, as version.cpp's voodoo requires it
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       7
-#define CLIENT_VERSION_REVISION    1
-#define CLIENT_VERSION_BUILD       9
+#define CLIENT_VERSION_REVISION    2
+#define CLIENT_VERSION_BUILD       0
 
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
@@ -32,7 +32,7 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60016;
+static const int PROTOCOL_VERSION = 60017;
 
 static const int MIN_PROTO_VERSION = 60005;
 // earlier versions not supported as of October 2013, and are disconnected
